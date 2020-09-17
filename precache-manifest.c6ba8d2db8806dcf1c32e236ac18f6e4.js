23e2432bde0ec72225182792d8209def861b7fc1
guldron-edu <guldron.edu@gmail.com>
2020-09-17 16:11:34 +0300

    Updates

---
+++ precache-manifest.c6ba8d2db8806dcf1c32e236ac18f6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3301daef26850a8b0400194098b665",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "1bdfba1c101daf64f566",
+    "url": "/goit-react-hw-01-components/static/css/main.4ed5c0d1.chunk.css"
+  },
+  {
+    "revision": "981dacd77a135d88ff96",
+    "url": "/goit-react-hw-01-components/static/js/2.03fc6e5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.03fc6e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdfba1c101daf64f566",
+    "url": "/goit-react-hw-01-components/static/js/main.3044d7e5.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.f89933310b5b1dd79c207e624cadb737.js"
+  "/goit-react-hw-01-components/precache-manifest.c6ba8d2db8806dcf1c32e236ac18f6e4.js"
 );
 
 self.addEventListener('message', (event) => {
